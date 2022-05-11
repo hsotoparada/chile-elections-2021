a9243005fab58108c1e93ba06a42c8b76019d6fd
hsotoparada <hugosotoparada@gmail.com>
2022-05-11 14:57:31 +0200

    Updated file

--- deploy/run_server.sh
+++ deploy/run_server.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # start nginx
-service nginx start
-# /usr/sbin/nginx
+# service nginx start
+/usr/sbin/nginx
 
 # start gunicorn
 gunicorn --bind :8080 --workers 5 --timeout 1000 --preload app:server
